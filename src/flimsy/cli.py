f4d475387a2c467af2d88b01c15f5ce3544079ec
n-garc <98439831+n-garc@users.noreply.github.com>
2025-11-29 23:16:00 -0700

    Working entry point with logging. Initial commits for submodules and some io utility functions

--- src/flimsy/cli.py
+++ src/flimsy/cli.py
@@ -1,9 +1,21 @@
+import os
+import logging
 import argparse
+from pathlib import Path
 
 from flimsy.pipeline import logging_utils
 
+logger = logging.getLogger(__name__)
+
 def get_parser():
     parser = argparse.ArgumentParser()
+
+    parser.add_argument('--logdir', required=False)
+
+    parser.add_argument('--overwrite', required=False, default='false', type=str, choices=['true', 'false', 'diff'])
+
+    parser.add_argument('--loglevel', default='info', choices=['debug', 'info', 'warning', 'error', 'critical'],
+            help='Set logging level (DEBUG, INFO, WARNING, ERROR, CRITICAL)')
 
     return parser
 
@@ -11,6 +23,11 @@ def main():
     parser = get_parser()
     args = parser.parse_args()
 
+    
+    Path(args.logdir, 'logs/').mkdir(parents=True, exist_ok=True) ## TODO -- maybe do this in logging? 
+    logging_utils.configure_logging(os.path.join(args.logdir, 'logs/'), args.loglevel)
+    logger.info('Welcome to flimsy - the Felsen Lab Integrated Modular analYsis package.')
+    logger.debug(f'args:\n{args}')
 
 if __name__ == "__main__":
     main()
--- src/flimsy/pipeline/logging_utils.py
+++ src/flimsy/pipeline/logging_utils.py
@@ -1,11 +1,18 @@
 import logging
 import os
-import datetime
+from datetime import datetime
 import colorlog
 
 logger = logging.getLogger(__name__)
 
-def configure_logging(log_dir: str, level=logging.INFO):
+def loglevel_type(level_str):
+    """Convert string to logging level constant."""
+    try:
+        return getattr(logging, level_str.upper())
+    except AttributeError:
+        raise argparse.ArgumentTypeError(f"Invalid log level: {level_str}")
+
+def configure_logging(log_dir: str, level='info'):
     """Configure the root logger for the pipeline."""
     log_format = '%(asctime)s - %(name)s - %(levelname)s - %(filename)s:%(lineno)d - %(message)s'
     current_time = datetime.now().strftime('%Y-%m-%d_%H.%M.%S')
@@ -24,12 +31,12 @@ def configure_logging(log_dir: str, level=logging.INFO):
                 'CRITICAL': 'white,bg_red'
         }))
 
-    if log_file is not None:
+    if log_dir is not None:
         log_file = os.path.join(log_dir, f'{current_time}.log')
         #Path(log_file).parent.mkdir(parents=True, exist_ok=True) ## TODO -- consider how we want to handle file creation
 
         file_handler = logging.FileHandler(log_file)
         file_handler.setFormatter(logging.Formatter(log_format))
 
-    logging.basicConfig(level=log_level, handlers=[file_handler, console_handler])
+    logging.basicConfig(level=logging.DEBUG, handlers=[file_handler, console_handler])
     logger.info('Logging to %s' % log_file)
