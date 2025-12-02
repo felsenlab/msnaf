97ac557fcccc7f37f5a057d68843ed246c6b1580
n-garc <98439831+n-garc@users.noreply.github.com>
2025-12-01 22:06:35 -0700

    initial commit and loose api for module system

---
+++ src/flimsy/pipeline/base.py
@@ -0,0 +1,18 @@
+# Modules are data-centric, atomic units designed to accomplish one task (e.g.,
+# analyseCameraWithLabjack or analyzeCameraWithoutLabjack). Modules specify 
+# inputs, outputs, and a name in module decorators. Modules are automatically
+# registered by these decorators with a central repository, which is used to 
+# construct and execute pipelines (see flimsy.pipeline.registry and 
+# flimsy.pipeline.pipeline for more information). ## TODO -- change pipeline to runner or something
+
+# Modules automatically perform basic validation of their output. For example,
+# modules will flag shape and datatype mismatches (e.g., 0 length arrays and 
+# arrays with type object or string when int/float was expected). They will
+# additionally check for nan/infs. Results of the validation are shown in the 
+# pipeline summary at the end of execution (in addition to major errors such 
+# as crashes)
+
+import logging
+
+logger = logging.getLogger(__name__)
+
